80382cd674d8d5fddfa3d5dba8cf816400f0272f
Dav He <s991179@gmail.com>
2023-12-31 23:59:46 +0800

    feat: benchmark test more

---
+++ generate2_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"math/rand"
+	"testing"
+	"time"
+)
+
+func generate2(n int) []int {
+	rand.Seed(time.Now().UnixNano())
+	nums := make([]int, 0)
+	for i := 0; i < n; i++ {
+		nums = append(nums, rand.Int())
+	}
+	return nums
+}
+func benchmarkGenerate(i int, b *testing.B) {
+	for n := 0; n < b.N; n++ {
+		generate2(i)
+	}
+}
+
+func BenchmarkGenerate1000(b *testing.B)    { benchmarkGenerate(1000, b) }
+func BenchmarkGenerate10000(b *testing.B)   { benchmarkGenerate(10000, b) }
+func BenchmarkGenerate100000(b *testing.B)  { benchmarkGenerate(100000, b) }
+func BenchmarkGenerate1000000(b *testing.B) { benchmarkGenerate(1000000, b) }
